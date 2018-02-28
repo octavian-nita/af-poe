9387a42e2d922cddbc7cf7c6a9c43279af67f5b0
Octavian Nita <octavian.nita@gmail.com>
2018-02-28 16:26:12 +0100

    clarifications

--- database/mariadb/V1.0.0/V1.0.0__02_create_schema_ldg.mariadb.sql
+++ database/mariadb/V1.0.0/V1.0.0__02_create_schema_ldg.mariadb.sql
@@ -1,25 +1,31 @@
 USE af_ledger;
 
+-- Entries posted in a very simple (household) ledger.
+--
 CREATE TABLE t_ldg_entry (
 
-  id         INTEGER UNSIGNED        NOT NULL UNIQUE AUTO_INCREMENT            COMMENT 'The entry technical identifier',
-  entry_date DATETIME                NOT NULL        DEFAULT CURRENT_TIMESTAMP COMMENT 'The entry creation date (may be different from the actual transaction date, usually soon after)',
-  amount     NUMERIC(19, 6) UNSIGNED NOT NULL        DEFAULT 0                 COMMENT 'The entry amount, always positive',
-  type       ENUM('CREDIT', 'DEBIT') NOT NULL        DEFAULT 'CREDIT'          COMMENT 'The entry type, either credit or debit',
-  foreseen   ENUM('Y', 'N')                                                    COMMENT 'Whether an entry was foreseen or not (if yes, some calculations might not consider it)',
-  details    VARCHAR(1000)                                                     COMMENT 'Additional / optional details on the specific entry',
+  id          INTEGER UNSIGNED        NOT NULL UNIQUE AUTO_INCREMENT            COMMENT 'The entry technical identifier',
+  posted_on   DATETIME                NOT NULL        DEFAULT CURRENT_TIMESTAMP COMMENT 'The date (and time) the entry has been posted on; may be different from the actual transaction date, usually soon after',
+  amount      NUMERIC(19, 6) UNSIGNED NOT NULL        DEFAULT 0                 COMMENT 'The amount of the transaction; always positive',
+  type        ENUM('DEBIT', 'CREDIT') NOT NULL        DEFAULT 'CREDIT'          COMMENT 'The entry type, either CREDIT or DEBIT',
+  foreseen    ENUM('Y', 'N')                                                    COMMENT 'Whether the entry was foreseen or not; if yes, some calculations might not consider it',
+  description VARCHAR(1000)                                                     COMMENT 'Optional description / details for the entry',
 
   PRIMARY KEY (id)
 ) AUTO_INCREMENT = 1;
 
+-- Categories for ledger entries.
+--
 CREATE TABLE t_ldg_category (
 
-  id      VARCHAR(255)  NOT NULL UNIQUE COMMENT 'A category code for an entry, e.g. OTHER, GROC from groceries, etc. (useful for reporting, grouping, insights, etc.); also a technical identifier',
-  details VARCHAR(1000)                 COMMENT 'Additional / optional details on the specific category',
+  id          VARCHAR(255)  NOT NULL UNIQUE COMMENT 'A category code for an entry, e.g. OTHER, GROC for groceries, etc. useful for reporting, grouping, insights, etc.; also a technical identifier',
+  description VARCHAR(1000)                 COMMENT 'Optional description / details for the category',
 
   PRIMARY KEY (id)
 );
 
+-- Associates ledger entries with categories (many-to-many).
+--
 CREATE TABLE t_ldg_entry_category (
 
   entry_id    INTEGER UNSIGNED NOT NULL UNIQUE,
