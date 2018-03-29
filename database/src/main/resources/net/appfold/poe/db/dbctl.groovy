c9adcff896510e86d9ee34dcc720ba068b071bc5
Octavian Nita <octavian.nita@gmail.com>
2018-03-29 17:41:55 +0200

    messy...

--- database/src/main/resources/net/appfold/poe/db/dbctl.groovy
+++ database/src/main/resources/net/appfold/poe/db/dbctl.groovy
@@ -1,19 +1,29 @@
 package net.appfold.poe.db
 
-switch (cmd = env('cmd').toLowerCase()) {
+import org.flywaydb.core.api.logging.LogFactory
+import org.flywaydb.core.internal.configuration.ConfigUtils
+import org.flywaydb.core.internal.util.logging.console.ConsoleLogCreator
 
-    case 'migrate':
-    case 'create':
-    case 'drop':
-        "$cmd"()
-        break
+import static org.flywaydb.core.api.logging.LogFactory.setFallbackLogCreator
+import static org.flywaydb.core.internal.util.ClassUtils.getLocationOnDisk
+import static org.flywaydb.core.internal.util.logging.console.ConsoleLog.Level.*
 
-    case '':
-        migrate()
-        break
+println new File(getLocationOnDisk(getClass())).parentFile.parentFile.absolutePath
 
-    default:
-        throw new RuntimeException("Database control command '${cmd}' not supported!")
+switch (cmd = env('db.cmd').toLowerCase()) {
+
+case 'migrate':
+case 'create':
+case 'drop':
+    "$cmd"()
+    break
+
+case '':
+    migrate()
+    break
+
+default:
+    throw new RuntimeException("Database control command '${cmd}' not supported!")
 }
 
 def migrate() { println 'migrate...' }
@@ -21,7 +31,7 @@ def migrate() { println 'migrate...' }
 def create() { println 'create...' }
 
 def drop() {
-    def confirm = env('confirm').toLowerCase()
+    def confirm = env('db.confirmDrop').toLowerCase()
     def final yes = ['y', 'yes', 'true']
 
     if (!(confirm in yes)) {
@@ -40,19 +50,25 @@ def drop() {
     }
 
     if (!(confirm in yes)) {
-        println "Use -Dconfirm=[${yes.join('|')}] to confirm database dropping"
+        println "Use -Ddb.confirmDrop=[${yes.join('|')}] to confirm database dropping"
         return 1
     }
 
     withDbAdminCredentials { username, password ->
         println "${username}:${password}"
-        println 'Dropping database...'
     }
 }
 
-def private withDbAdminCredentials(callback) {
-    String username = env('dbAdminUsername')
-    char[] password = env('dbAdminPassword').getBytes()
+private env(String name, String defaultValue = '') {
+    ((System.properties[name] ?: System.getenv(name) ?: binding.variables[name] ?:
+                                                        binding.variables.project?.properties?.get(name) ?:
+                                                        binding.variables.project?."${name}" ?:
+                                                        defaultValue) as String).trim()
+}
+
+private withDbAdminCredentials(callback) {
+    String username = env('db.adminUsername')
+    char[] password = env('db.adminPassword').getBytes()
 
     def console = System.console()
     if (console) {
@@ -76,6 +92,40 @@ def private withDbAdminCredentials(callback) {
     }
 }
 
-def private env(String name, String defaultValue = '') {
-    ((binding.variables[name] ?: System.properties[name] ?: defaultValue) as String).trim()
+private Properties loadFlywayConfig() {
+    def final properties = new Properties()
+
+    def flywayEnv = ConfigUtils.environmentVariablesToPropertyMap()
+
+    def final basedir = new File(getLocationOnDisk(getClass())).parentFile.parentFile.absolutePath
+
+    //def workDir = env('')  workingDirectory == null ? mavenProject.getBasedir() : workingDirectory;
+
+    properties.put(ConfigUtils.LOCATIONS, "filesystem:" + new File(getInstallationDir(), "sql").getAbsolutePath());
+    properties.put(ConfigUtils.JAR_DIRS, new File(getInstallationDir(), "jars").getAbsolutePath());
+
+    loadConfigurationFromConfigFiles(properties, args, envVars);
+    properties.putAll(envVars);
+    overrideConfigurationWithArgs(properties, args);
+
+    filterProperties(properties);
+
+    properties
+}
+
+private initFlywayLogging() {
+    def level
+    switch (env('flyway.logLevel')) {
+    case '-X':
+        level = DEBUG
+        break
+    case '-q':
+        level = WARN
+        break
+    default:
+        level = INFO
+        break
+    }
+    setFallbackLogCreator(new ConsoleLogCreator(level))
+    LogFactory.getLog(getClass());
 }
