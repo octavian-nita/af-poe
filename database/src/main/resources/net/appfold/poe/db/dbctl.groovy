bef04eb92c018c7273083cb4a1e9cda8a5275c09
octavian <octavian.nita@gmail.com>
2018-04-04 02:10:26 +0200

    configuration loading done

--- database/src/main/resources/net/appfold/poe/db/dbctl.groovy
+++ database/src/main/resources/net/appfold/poe/db/dbctl.groovy
@@ -1,34 +1,38 @@
 package net.appfold.poe.db
 
-import org.flywaydb.core.api.logging.Log
-import org.flywaydb.core.api.logging.LogFactory
 import org.flywaydb.core.internal.configuration.ConfigUtils
 import org.flywaydb.core.internal.util.logging.console.ConsoleLogCreator
 
 import java.nio.file.Files
 import java.nio.file.Paths
 
+import static java.lang.System.*
 import static java.lang.reflect.Modifier.*
+import static org.flywaydb.core.api.logging.LogFactory.getLog
 import static org.flywaydb.core.api.logging.LogFactory.setFallbackLogCreator
 import static org.flywaydb.core.internal.configuration.ConfigUtils.*
+import static org.flywaydb.core.internal.util.Location.FILESYSTEM_PREFIX
+import static org.flywaydb.core.internal.util.StringUtils.tokenizeToStringArray
+import static org.flywaydb.core.internal.util.StringUtils.trimOrPad
 import static org.flywaydb.core.internal.util.logging.console.ConsoleLog.Level.*
 
+initFlywayLogging()
 switch (cmd = cfg('db.cmd').toLowerCase()) {
 
 case 'create':
 case 'drop':
-    "$cmd"()
+    "$cmd"(loadFlywayConfig())
     break
 
 default:
     err.println 'Use -Ddb.cmd=[create|drop] to create or drop, respectively, the database schema and main user'
-    throw cmd ? new RuntimeException("Database control command '${cmd}' not supported!")
-              : new RuntimeException('No database control command has been specified!')
+    throw new RuntimeException(
+        cmd ? "Database control command '${cmd}' not supported!" : 'No database control command has been specified!')
 }
 
-def create() { println 'create...' }
+def create(Properties config) { println 'create...' }
 
-def drop() {
+def drop(Properties config) {
     def confirm = cfg('db.confirmDrop').toLowerCase()
     def final yes = ['y', 'yes', 'true']
 
@@ -41,36 +45,26 @@ def drop() {
         println '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
         println ''
 
-        def console = System.console()
+        def console = console()
         if (console) {
             confirm = console.readLine('Drop the database?  [y/N]: ').trim().toLowerCase()
         }
     }
 
     if (!(confirm in yes)) {
-        System.err.println "Use -Ddb.confirmDrop=[${yes.join('|')}] to confirm database dropping"
+        err.println "Use -Ddb.confirmDrop=[${yes.join('|')}] to confirm database dropping"
         return 1
     }
 
-    withDbAdminCredentials { username, password -> println "${username}:${password}" }
-}
-
-private String cfg(String name, def defaultOrProvider = '') {
-    // Maven / Gradle project passed in bindings?
-    def final project = binding.variables.project
-    return ((System.properties[name] ?: project?.properties?.get(name) ?:
-                                        project?.hasProperty(name)?.getProperty(project) ?:
-                                        binding.variables[name] ?: System.getenv(name) ?:
-                                                                   (defaultOrProvider instanceof Closure ?
-                                                                    defaultOrProvider.call() :
-                                                                    defaultOrProvider)) as String).trim()
+    withDbAdminCredentials { username, password -> println "${username}:${password}"
+    }
 }
 
 private withDbAdminCredentials(callback) {
     String username = cfg('db.adminUsername')
     char[] password = cfg('db.adminPassword').getBytes()
 
-    def console = System.console()
+    def console = console()
     if (console) {
         if (!username) {
             username = console.readLine('DB admin username [admin]: ').trim()
@@ -90,6 +84,77 @@ private withDbAdminCredentials(callback) {
             Arrays.fill(password, ' ' as char)
         }
     }
+}
+
+private initFlywayLogging() {
+    def level
+    switch (cfg('flyway.logLevel')) {
+    case ['X', '-X']:
+        level = DEBUG
+        break
+    case ['q', '-q']:
+        level = WARN
+        break
+    default:
+        level = INFO
+        break
+    }
+    setFallbackLogCreator(new ConsoleLogCreator(level))
+}
+
+private Properties loadFlywayConfig() {
+    def final config = new Properties()
+    def final cfgEnc = cfg(CONFIG_FILE_ENCODING, 'UTF-8')
+    def final loadOpt = ConfigUtils.&loadConfigurationFile.rcurry(cfgEnc, false)
+    def final loadMnd = ConfigUtils.&loadConfigurationFile.rcurry(cfgEnc, true)
+
+    // Defaults:
+    config.put(LOCATIONS,
+               cfg(LOCATIONS, { FILESYSTEM_PREFIX + absolutePath(cfg('db.dialect'), 'migration', Files.&isDirectory) }))
+
+    // Configuration files:
+    config.putAll(loadOpt(new File(absolutePath('conf', CONFIG_FILE_NAME))))
+    config.putAll(loadOpt(new File(properties['user.home'] as String, CONFIG_FILE_NAME)))
+    config.putAll(loadOpt(new File(CONFIG_FILE_NAME)))
+
+    def configFiles = cfg(CONFIG_FILE)
+    if (configFiles) {
+        getLog(getClass()).warn('configFile is deprecated and will be removed in Flyway 6.0. Use configFiles instead.')
+        config.putAll(loadMnd(new File(configFiles)))
+    }
+    configFiles = cfg(CONFIG_FILES)
+    if (configFiles) {
+        for (String configFile : tokenizeToStringArray(configFiles, ',')) {
+            config.putAll(loadMnd(new File(configFile)))
+        }
+    }
+
+    // Environment variables & arguments:
+    ConfigUtils.class.declaredFields.each { field ->
+        def final mod = field.modifiers
+        if (String.class == field.getType() && isPublic(mod) && isStatic(mod) && isFinal(mod)) {
+            def key = field.get(null) as String
+            def val = cfg(key, '')
+            if (val) {
+                config.put(key, val)
+            }
+        }
+    }
+
+    config.remove(JAR_DIRS)
+    config.remove(CONFIG_FILE)
+    config.remove(CONFIG_FILES)
+    config.remove(CONFIG_FILE_ENCODING)
+
+    def final log = getLog(getClass())
+    log.debug("Using configuration:")
+    for (def entry : config.entrySet()) {
+        def value = entry.value as String
+        value = PASSWORD == entry.key ? trimOrPad('', value.length(), '*' as char) : value
+        log.debug("${entry.key} -> ${value}")
+    }
+
+    return config
 }
 
 private String absolutePath(String parent = '', String child = '', Closure<?> test = Files.&isRegularFile) {
@@ -124,51 +189,15 @@ private String absolutePath(String parent = '', String child = '', Closure<?> te
     return Paths.get(basedir, parent, child).toAbsolutePath().normalize() as String
 }
 
-private Log initFlywayLogging() {
-    def level
-    switch (cfg('flyway.logLevel')) {
-    case '-X':
-        level = DEBUG
-        break
-    case '-q':
-        level = WARN
-        break
-    default:
-        level = INFO
-        break
-    }
-    setFallbackLogCreator(new ConsoleLogCreator(level))
-    return LogFactory.getLog(getClass());
-}
+private String cfg(String name, def defaultOrProvider = '') {
+    // Maven / Gradle project passed in bindings?
+    def final project = binding.variables.project
 
-private Properties loadFlywayConfig() {
-    def final config = new Properties()
-    def final load = ConfigUtils.&loadConfigurationFile.rcurry(cfg(CONFIG_FILE_ENCODING, 'UTF-8'), false)
-
-    // Defaults:
-    config.put(LOCATIONS,
-               cfg(LOCATIONS, { 'filesystem:' + absolutePath(cfg('db.dialect'), 'migration', Files.&isDirectory) }))
-
-    // Configuration files:
-    config.putAll(load(new File(absolutePath('conf', CONFIG_FILE_NAME))))
-    config.putAll(load(new File(System.properties['user.home'] as String, CONFIG_FILE_NAME)))
-    config.putAll(load(new File(CONFIG_FILE_NAME)))
-
-    //for (File configFile : determineConfigFilesFromArgs(args, envVars)) {
-    //    config.putAll(loadConfigurationFile(configFile, encoding, true));
-    //}
-
-    // Environment variables & arguments:
-    ConfigUtils.class.declaredFields.each { field ->
-        def final mod = field.modifiers
-        if (String.class == field.getType() && isPublic(mod) && isStatic(mod) && isFinal(mod)) {
-            def key = field.get(null) as String
-            def val = cfg(key, '')
-            if (val) {
-                config.put(key, val)
-            }
-        }
-    }
-
-    return config
+    //@fmt:off
+    return ((properties[name]                                 ?:
+             project?.properties?.get(name)                   ?:
+             project?.hasProperty(name)?.getProperty(project) ?:
+             binding.variables[name]                          ?:
+             getenv(name)                                     ?://@fmt:on
+             (defaultOrProvider instanceof Closure ? defaultOrProvider.call() : defaultOrProvider)) as String).trim()
 }
