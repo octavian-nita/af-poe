8b8488df5432cc36a7bfe2adb3f09c1a0a5c857b
Octavian Nita <octavian.nita@gmail.com>
2018-04-04 17:17:16 +0200

    clean, install goals almost completed

--- database/src/main/resources/net/appfold/poe/db/dbctl.groovy
+++ database/src/main/resources/net/appfold/poe/db/dbctl.groovy
@@ -1,8 +1,14 @@
 package net.appfold.poe.db
 
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.configuration.FlywayConfiguration
 import org.flywaydb.core.internal.configuration.ConfigUtils
+import org.flywaydb.core.internal.util.StringUtils
+import org.flywaydb.core.internal.util.jdbc.DriverDataSource
 import org.flywaydb.core.internal.util.logging.console.ConsoleLogCreator
+import org.flywaydb.core.internal.util.scanner.filesystem.FileSystemResource
 
+import javax.sql.DataSource
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -11,9 +17,8 @@ import static java.lang.reflect.Modifier.*
 import static org.flywaydb.core.api.logging.LogFactory.getLog
 import static org.flywaydb.core.api.logging.LogFactory.setFallbackLogCreator
 import static org.flywaydb.core.internal.configuration.ConfigUtils.*
+import static org.flywaydb.core.internal.database.DatabaseFactory.createDatabase
 import static org.flywaydb.core.internal.util.Location.FILESYSTEM_PREFIX
-import static org.flywaydb.core.internal.util.StringUtils.tokenizeToStringArray
-import static org.flywaydb.core.internal.util.StringUtils.trimOrPad
 import static org.flywaydb.core.internal.util.logging.console.ConsoleLog.Level.*
 
 initFlywayLogging()
@@ -30,11 +35,17 @@ default:
         cmd ? "Database control command '${cmd}' not supported!" : 'No database control command has been specified!')
 }
 
-def create(Properties config) { println 'create...' }
+def create(FlywayConfiguration config) {
+    final dbDialect = cfg('db.dialect')
+    asDbAdmin(config) { DataSource adminDataSource ->
+        executeSqlScript(absolutePath(dbDialect, "create_db${dbDialect ? '.' + dbDialect : ''}.sql"), config,
+                         adminDataSource)
+    }
+}
 
-def drop(Properties config) {
+def drop(FlywayConfiguration config) {
     def confirm = cfg('db.confirmDrop').toLowerCase()
-    def final yes = ['y', 'yes', 'true']
+    final yes = ['y', 'yes', 'true']
 
     if (!(confirm in yes)) {
         println ''
@@ -56,32 +67,54 @@ def drop(Properties config) {
         return 1
     }
 
-    withDbAdminCredentials { username, password -> println "${username}:${password}"
+    final dbDialect = cfg('db.dialect')
+    asDbAdmin(config) { DataSource adminDataSource ->
+        executeSqlScript(absolutePath(dbDialect, "drop_db${dbDialect ? '.' + dbDialect : ''}.sql"), config,
+                         adminDataSource)
     }
 }
 
-private withDbAdminCredentials(callback) {
-    String username = cfg('db.adminUsername')
-    char[] password = cfg('db.adminPassword').getBytes()
+private executeSqlScript(String sqlScriptLocation, FlywayConfiguration config, DataSource dataSource = null) {
+    final scriptResource = new FileSystemResource(sqlScriptLocation)
+
+    createDatabase(dataSource ? new groovy.util.Proxy() {
+
+        DataSource getDataSource() { dataSource }
+
+    }.wrap(config) as FlywayConfiguration : config, true).withCloseable { db ->
+
+        db.createSqlScript(scriptResource, scriptResource.loadAsString(config.encoding ?: 'UTF-8'),
+                           config.mixed).execute(db.mainConnection.jdbcTemplate)
+    }
+}
+
+private asDbAdmin(FlywayConfiguration config, Closure callback) {
+    String adminUsername = cfg('db.adminUsername')
+    char[] adminPassword = cfg('db.adminPassword').bytes
 
     def console = console()
     if (console) {
-        if (!username) {
-            username = console.readLine('DB admin username [admin]: ').trim()
-            if (!username) {
-                username = 'admin'
+        if (!adminUsername) {
+            adminUsername = console.readLine('DB admin username [admin]: ').trim()
+            if (!adminUsername) {
+                adminUsername = 'admin'
             }
         }
-        if (!password) {
-            password = console.readPassword('DB admin password: ')
+        if (!adminPassword) {
+            adminPassword = console.readPassword('DB admin password: ')
         }
     }
 
     try {
-        callback(username, password)
+        // TODO we might attempt to autodetect the driver by default, based on the URL...
+        callback(new DriverDataSource(config?.classLoader ?: getClass().classLoader,
+                                      cfg('db.jdbcDriver', { cfg('flyway.driver') }),
+                                      cfg('db.jdbcServerUrl',
+                                          { "jdbc:${cfg('db.dialect')}://${cfg('db.host')}:${cfg('db.port')}" }),
+                                      adminUsername, new String(adminPassword)))
     } finally {
-        if (password) {
-            Arrays.fill(password, ' ' as char)
+        if (adminPassword) {
+            Arrays.fill(adminPassword, ' ' as char)
         }
     }
 }
@@ -102,11 +135,11 @@ private initFlywayLogging() {
     setFallbackLogCreator(new ConsoleLogCreator(level))
 }
 
-private Properties loadFlywayConfig() {
-    def final config = new Properties()
-    def final cfgEnc = cfg(CONFIG_FILE_ENCODING, 'UTF-8')
-    def final loadOpt = ConfigUtils.&loadConfigurationFile.rcurry(cfgEnc, false)
-    def final loadMnd = ConfigUtils.&loadConfigurationFile.rcurry(cfgEnc, true)
+private FlywayConfiguration loadFlywayConfig() {
+    final config = new Properties()
+    final cfgEnc = cfg(CONFIG_FILE_ENCODING, 'UTF-8')
+    final loadOpt = ConfigUtils.&loadConfigurationFile.rcurry(cfgEnc, false)
+    final loadMnd = ConfigUtils.&loadConfigurationFile.rcurry(cfgEnc, true)
 
     // Defaults:
     config.put(LOCATIONS,
@@ -124,15 +157,15 @@ private Properties loadFlywayConfig() {
     }
     configFiles = cfg(CONFIG_FILES)
     if (configFiles) {
-        for (String configFile : tokenizeToStringArray(configFiles, ',')) {
+        for (String configFile : StringUtils.tokenizeToStringArray(configFiles, ',')) {
             config.putAll(loadMnd(new File(configFile)))
         }
     }
 
     // Environment variables & arguments:
     ConfigUtils.class.declaredFields.each { field ->
-        def final mod = field.modifiers
-        if (String.class == field.getType() && isPublic(mod) && isStatic(mod) && isFinal(mod)) {
+        final mod = field.modifiers
+        if (String.class == field.type && isPublic(mod) && isStatic(mod) && isFinal(mod)) {
             def key = field.get(null) as String
             def val = cfg(key, '')
             if (val) {
@@ -146,15 +179,21 @@ private Properties loadFlywayConfig() {
     config.remove(CONFIG_FILES)
     config.remove(CONFIG_FILE_ENCODING)
 
-    def final log = getLog(getClass())
-    log.debug("Using configuration:")
+    // Dump configuration (debug purposes)
+    final log = getLog(getClass())
+    log.debug('Using configuration:')
     for (def entry : config.entrySet()) {
         def value = entry.value as String
-        value = PASSWORD == entry.key ? trimOrPad('', value.length(), '*' as char) : value
+        value = PASSWORD == entry.key ? StringUtils.trimOrPad('', value.length(), '*' as char) : value
         log.debug("${entry.key} -> ${value}")
     }
 
-    return config
+    // In Flyway versions prior to 6, org.flywaydb.core.Flyway seems to be the only class implementing the
+    // org.flywaydb.core.api.configuration.FlywayConfiguration interface hence the easiest way to obtain a
+    // configuration is as follows:
+    final Flyway flyway = new Flyway()
+    flyway.configure(config)
+    return flyway
 }
 
 private String absolutePath(String parent = '', String child = '', Closure<?> test = Files.&isRegularFile) {
@@ -162,7 +201,7 @@ private String absolutePath(String parent = '', String child = '', Closure<?> te
     child = child ?: ''
     test = test ?: Files.&isRegularFile
 
-    def final basedir = cfg('basedir', { Paths.get('') as String })
+    final basedir = cfg('basedir', { Paths.get('') as String })
 
     def path = Paths.get(basedir, parent, child)
     if (test(path)) {
@@ -174,7 +213,7 @@ private String absolutePath(String parent = '', String child = '', Closure<?> te
         return path.toAbsolutePath().normalize() as String
     }
 
-    def final packagePath = [*getClass().package.name.split('\\.'), parent, child]
+    final packagePath = [*getClass().package.name.split('\\.'), parent, child]
 
     path = Paths.get(basedir, *packagePath) // under package / namespace?
     if (test(path)) {
@@ -191,7 +230,7 @@ private String absolutePath(String parent = '', String child = '', Closure<?> te
 
 private String cfg(String name, def defaultOrProvider = '') {
     // Maven / Gradle project passed in bindings?
-    def final project = binding.variables.project
+    final project = binding.variables.project
 
     //@fmt:off
     return ((properties[name]                                 ?:
