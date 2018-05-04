0282ac993e4e9a2b54981bbe30cc51cdbf2b18ed
Octavian Nita <octavian.nita@gmail.com>
2018-05-04 15:19:47 +0200

    added beneficiary to the expense journal

--- database/src/main/resources/net/appfold/poe/db/mariadb/migration/V1.0.0/V1.0.0_01__create_expense_journal.mariadb.sql
+++ database/src/main/resources/net/appfold/poe/db/mariadb/migration/V1.0.0/V1.0.0_01__create_expense_journal.mariadb.sql
@@ -4,10 +4,11 @@
 CREATE TABLE journal_entry (
 
     id          INTEGER UNSIGNED        NOT NULL UNIQUE AUTO_INCREMENT            COMMENT 'The entry technical identifier',
-    posted_on   DATETIME                NOT NULL        DEFAULT CURRENT_TIMESTAMP COMMENT 'The date (and time) the entry has been posted on; may be different from the actual transaction date, usually soon after',
-    amount      NUMERIC(19, 6) UNSIGNED NOT NULL        DEFAULT 0                 COMMENT 'The amount of the transaction; always positive',
-    type        ENUM('DEBIT', 'CREDIT') NOT NULL        DEFAULT 'CREDIT'          COMMENT 'The entry type, either DEBIT or CREDIT',
-    foreseen    ENUM('Y', 'N')                                                    COMMENT 'Whether the entry was foreseen or not; if yes, some calculations might discard the entry amount',
+    posted_on   DATETIME                NOT NULL        DEFAULT CURRENT_TIMESTAMP COMMENT 'The date and time the entry was posted; may differ from the actual transaction date (usually soon after)',
+    amount      NUMERIC(19, 6) UNSIGNED NOT NULL        DEFAULT 0                 COMMENT 'The transaction amount; always positive',
+    type        ENUM('DEBIT', 'CREDIT') NOT NULL        DEFAULT 'CREDIT'          COMMENT 'The transaction type, either DEBIT or CREDIT',
+    foreseen    ENUM('Y', 'N')                          DEFAULT 'N'               COMMENT 'Whether the entry was foreseen or not; if yes, some calculations might discard the entry amount',
+    beneficiary VARCHAR(256)                                                      COMMENT 'The person who benefits from the expenditure, if it can be established',
     description VARCHAR(1000)                                                     COMMENT 'Optional description / details for the entry',
 
     PRIMARY KEY (id)
@@ -18,8 +19,8 @@ CREATE TABLE journal_entry (
 --
 CREATE TABLE entry_category (
 
-    id          VARCHAR(24)  NOT NULL UNIQUE COMMENT 'A category code for an entry, e.g. OTHER, GROC for groceries... useful for reporting, grouping, insights, etc.; also a technical identifier',
-    description VARCHAR(1000)                COMMENT 'Optional description / details for the category',
+    id          VARCHAR(24)   NOT NULL UNIQUE COMMENT 'A category code for an entry, e.g. OTHER, GROC for groceries... useful for reporting, grouping, insights, etc.; also a technical identifier',
+    description VARCHAR(1000)                 COMMENT 'Optional description / details for the category',
 
     PRIMARY KEY (id)
 );
